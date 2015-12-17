9fef942c81fc6b6b3efe0f42a5f998e5ff83db2c
David McGinn <davidmcginn@gmail.com>
2015-12-17 12:41:44 +0000

    Add more tests

--- test/test_number_converter.rb
+++ test/test_number_converter.rb
@@ -9,11 +9,51 @@ class NumberConverterTest < Minitest::Test
       'LXIV' => 64,
       'CCXXVI' => 226,
       'CM' => 900,
-      'CMXCVIII' => 998, 'MDCCXII' => 1712
+      'CMXCVIII' => 998, 
+      'MDCCXII' => 1712,
+      'MDCLXXXVII' => 1687,
+      'DCLXVI' => 666,
+      'MCCXII' => 1212,
+      'CMXLVIII' => 948,
+      'MCMXLII' => 1942
     }
 
     valid_numerals.each { |roman_numeral, number|
-      assert_equal roman_numeral, NumberConverter.new(number).convert('roman')
+      assert_equal roman_numeral, NumberConverter.new(number).convert_to('roman')
     }
+  end
+
+  def test_invalid_argument_to_convert
+    invalid_arguments = ['ABC', '!123', '567']
+
+    invalid_arguments.each { |arg|
+      exception = assert_raises(Exception) {
+        number = NumberConverter.new(arg)
+        number.convert_to('roman') # Should raise exception at this point
+      }
+      assert_equal(exception.message, "Argument is not an integer")
+    }
+  end
+
+  def test_invalid_conversion_format
+    number = 1712
+    roman_numeral = 'MDCCXII'
+
+    # Format argument is case insensitive
+    assert_equal(roman_numeral, NumberConverter.new(number).convert_to('RoMaN'))
+
+    exception = assert_raises(Exception) {
+      NumberConverter.new(number).convert_to('roman_numerals')
+    }
+
+    assert_equal("Invalid conversion format", exception.message)
+  end
+
+  def test_zero
+    exception = assert_raises(Exception) {
+      NumberConverter.new(0).convert_to('roman')
+    }
+
+    assert_equal("There is no Roman numeral for zero", exception.message)
   end
 end
